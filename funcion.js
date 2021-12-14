4142a44445d3db58235fcc319640ca37d28d526f
Axeljair <89996055+Axeljair@users.noreply.github.com>
2021-12-13 20:00:03 -0600

    Create funcion.js

---
+++ funcion.js
@@ -0,0 +1,14 @@
+función  hola  ( ) {
+    consola . log ( "Hola" )
+}
+function  ce  ( ) {
+    consola . log ( "¿Como estas?" )
+}
+
+función  adios  ( ) {
+    consola . log ( "Adiós" )
+}
+
+hola ( ) ;
+ce ( ) ;
+adios ( ) ;
